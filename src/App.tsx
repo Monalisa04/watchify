498349b57ea321a05eb4ce767efcae89e2b9b691
Monalisa04 <monalisa.svnit@gmail.com>
2020-07-07 01:28:55 +0100

    Updating Styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/navbar";
 import SideNav from "./components/sidenav/sidenav";
 import "./assets/styles/app.scss";
 import AppContent from "./components/app-content/app-content";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <SideNav />
         <AppContent />
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+const Footer = () => <footer className="app-footer"></footer>;
+export default Footer;
